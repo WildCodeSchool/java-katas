d8612bdf7dcf59017a69022718b912543614b27a
bastienwcs <email>
2020-01-21 09:33:39 +0100

    init

---
+++ src/kata/Censorship.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+public class Censorship {
+
+    // TODO
+    public static String[] censor(List<String> sentences, String word) {
+
+        return new String[]{};
+    }
+}

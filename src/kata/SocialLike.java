08d227993e66f5b5747a5ebc9a71a3f5372f5743
bastienwcs <bastien@wildcodeschool.fr>
2020-03-23 15:28:32 +0100

    solution

--- src/kata/SocialLike.java
+++ src/kata/SocialLike.java
@@ -1,4 +1,17 @@
 public class SocialLike {
 
-    // TODO
+    public static String like(String[] names) {
+        
+        if (names.length == 1) {
+            return names[0] + " like this";
+        }
+        if (names.length == 2) {
+            return names[0] + " and " + names[1] + " like this";
+        }
+        if (names.length > 2) {
+            return names[0] + " and " + (names.length - 1) + " other people like this";
+        }
+
+        return "Be the first to like this";
+    }
 }
--- src/test/SocialLikeTest.java
+++ src/test/SocialLikeTest.java
@@ -1,12 +1,30 @@
 import org.junit.Assert;
 import org.junit.Test;
 
-public class MainTest {
+public class SocialLikeTest {
 
     @Test
     public void testTrue() throws Exception {
         Assert.assertEquals(true, true);
     }
 
-    // TODO
+    @Test
+    public void testFirst() throws Exception {
+        Assert.assertEquals("Be the first to like this", SocialLike.like(new String[]{}));
+    }
+
+    @Test
+    public void testOne() throws Exception {
+        Assert.assertEquals("Cartman like this", SocialLike.like(new String[]{"Cartman"}));
+    }
+
+    @Test
+    public void testTwo() throws Exception {
+        Assert.assertEquals("Kenny and Cartman like this", SocialLike.like(new String[]{"Kenny", "Cartman"}));
+    }
+
+    @Test
+    public void testMore() throws Exception {
+        Assert.assertEquals("Stan and 3 other people like this", SocialLike.like(new String[]{"Stan", "Kyle", "Kenny", "Cartman"}));
+    }
 }

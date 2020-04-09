9d866ccd93f794a79f1173cac0ce7d0d168dcbcf
bastienwcs <bastien@wldcodeschool.fr>
2020-04-09 10:37:58 +0200

    add test

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Pyradmid
 
-Create a method that returns a pyramid of `n` floors, from top to bottom, stored in a `String` array.
+Create a method `draw` that returns a pyramid of `n` floors, from top to bottom, stored in a `String` array.
 
 *E.g :*
 
--- src/test/PyramidTest.java
+++ src/test/PyramidTest.java
@@ -8,5 +8,38 @@ public class PyramidTest {
         Assert.assertEquals(true, true);
     }
 
-    // TODO
+    @Test
+    public void test0() throws Exception {
+        String[] expected = {};
+        Assert.assertArrayEquals(expected, Pyramid.draw(0));
+    }
+
+    @Test
+    public void test1() throws Exception {
+        String[] expected = {
+                "*"
+        };
+        Assert.assertArrayEquals(expected, Pyramid.draw(1));
+    }
+
+    @Test
+    public void test2() throws Exception {
+        String[] expected = {
+                " * ",
+                "***"
+        };
+        Assert.assertArrayEquals(expected, Pyramid.draw(2));
+    }
+
+    @Test
+    public void test5() throws Exception {
+        String[] expected = {
+                "    *    ",
+                "   ***   ",
+                "  *****  ",
+                " ******* ",
+                "*********",
+        };
+        Assert.assertArrayEquals(expected, Pyramid.draw(5));
+    }
 }

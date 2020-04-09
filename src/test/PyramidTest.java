0c738b0eebd800b040f56d940040b5be0d6d9422
bastienwcs <bastien@wldcodeschool.fr>
2020-04-09 10:39:39 +0200

    add test

--- src/test/PyramidTest.java
+++ src/test/PyramidTest.java
@@ -8,6 +8,28 @@ public class PyramidTest {
         Assert.assertEquals(true, true);
     }
 
+    @Test
+    public void test0() throws Exception {
+        String[] expected = {};
+        Assert.assertArrayEquals(expected, Pyramid.draw(0));
+    }
+
+    @Test
+    public void test1() throws Exception {
+        String[] expected = {
+                "*"
+        };
+        Assert.assertArrayEquals(expected, Pyramid.draw(1));
+    }
+
+    @Test
+    public void test2() throws Exception {
+        String[] expected = {
+                " * ",
+                "***"
+        };
+        Assert.assertArrayEquals(expected, Pyramid.draw(2));
+    }
 
     @Test
     public void test5() throws Exception {

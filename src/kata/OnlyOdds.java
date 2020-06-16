fef6891cb9154a9881fba0b85fe7dec383d26663
bastienwcs <bastien@wldcodeschool.fr>
2020-06-16 10:41:17 +0200

    fix

--- src/kata/OnlyOdds.java
+++ src/kata/OnlyOdds.java
@@ -1,3 +1,7 @@
+import java.util.*;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
 public class OnlyOdds {
 
     // TODO
--- src/test/OnlyOddsTest.java
+++ src/test/OnlyOddsTest.java
@@ -1,5 +1,6 @@
 import org.junit.Assert;
 import org.junit.Test;
+import java.util.*;
 
 public class OnlyOddsTest {
 
